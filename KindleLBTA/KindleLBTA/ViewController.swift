ad1a9db7602b5e19db1807b17269c1d82f303838
Maksim Nosov <ozolc@yandex.ru>
2019-03-21 22:54:46 +0300

    Optionals

--- KindleLBTA/KindleLBTA/ViewController.swift
+++ KindleLBTA/KindleLBTA/ViewController.swift
@@ -31,35 +31,53 @@ class Page {
 }
 
 class ViewController: UIViewController {
-
+    
+    var books: [Book]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .red
         // can provide custom code starting here
         
+        setupBooks()        
+    }
+    
+    func setupBooks() {
         let page1 = Page(number: 1, text: "Text for the first page")
         let page2 = Page(number: 2, text: "Text for the second page")
         
         let pages = [page1, page2]
         
         let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
-                
+        
         let book2 = Book(title: "Bill Gates: A Biography", author: "Michael Becraft", pages: [
             Page(number: 1, text: "Text for page 1"),
             Page(number: 2, text: "Text for page 2"),
             Page(number: 3, text: "Text for page 3"),
             Page(number: 4, text: "Text for page 4")
             ])
-
-        for book in [book, book2] {
-            print(book.title)
-            for page in book.pages {
-                print(page.text)
+        
+        self.books = [book, book2]
+        
+        guard let books = self.books else { return }
+        
+        //        for book in books {
+        //            print(book.title)
+        //            for page in book.pages {
+        //                print(page.text)
+        //            }
+        //        }
+        
+        if let unwrappedBooks = self.books {
+            for book in unwrappedBooks {
+                print(book.title)
+                for page in book.pages {
+                    print(page.text)
+                }
             }
         }
     }
-
-
+    
 }
 
