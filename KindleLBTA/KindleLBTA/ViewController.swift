4d33f018d6d356070b9f91346b4e3283514a2845
Maksim Nosov <ozolc@yandex.ru>
2019-03-29 18:50:18 +0300

    Custom Book Constructor

--- KindleLBTA/KindleLBTA/ViewController.swift
+++ KindleLBTA/KindleLBTA/ViewController.swift
@@ -42,13 +42,10 @@ class ViewController: UITableViewController {
                     self.books = []
                     
                     for bookDictionary in bookDictionaries {
-                        if let title = bookDictionary["title"] as? String,
-                            let author = bookDictionary["author"] as? String
-                        {
-                            let book = Book(title: title, author: author, image: #imageLiteral(resourceName: "steve_jobs"), pages: [])
-                            self.books?.append(book)
-                        }
+                        let book = Book(dictionary: bookDictionary)
+                        self.books?.append(book)
                     }
+                    
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
                     }
