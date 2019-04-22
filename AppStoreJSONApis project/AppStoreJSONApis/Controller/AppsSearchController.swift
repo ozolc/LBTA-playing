c64b876467b76896227af613866c5a0b388567cb
Maksim Nosov <ozolc@yandex.ru>
2019-04-22 20:07:47 +0300

    06 - iTunes Search API JSON Fetch

--- AppStoreJSONApis project/AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis project/AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -18,8 +18,40 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         
         collectionView.backgroundColor = .white
         
-        // Регистрируем класс (указать Class.self) для использования при создании но ячейки CollectionView (обязательно регистрировать!!!)
+        // Регистрируем класс (указать Class.self) для использования при создании ячейки CollectionView (обязательно регистрировать!!!)
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+        
+        fetchITunesApps()
+    }
+    
+    fileprivate func fetchITunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        // Получение данных из Интернет
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            
+            // Проверка на получение ошибки при загрузки из Интернет. Если ошибка - выход из метода.
+            if let err = err {
+                print("Failed to fetch apps:", err)
+                return
+            }
+            
+            // Успех загрузки
+            
+            guard let data = data else { return }
+            
+            // Пытаемся произвести декодирование из JSON в объект структуры SearchResult.
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                
+                searchResult.results.forEach { print($0.trackName, $0.primaryGenreName) }
+                
+            } catch let jsonErr {
+                print("Failed to decode json:", jsonErr)
+            }
+            
+        }.resume() // Запустить получение данных из URL. Иначе загрузка не будет произведена.
     }
     
     // В этом методе мы переопределяем размер ячейки (метод из протокола UICollectionViewDelegateFlowLayout)
@@ -33,8 +65,8 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = "Here is my app name"
         return cell
     }
     
---
+++ AppStoreJSONApis project/AppStoreJSONApis/Model/SearchResult.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResult.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 22/04/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+// Структуры для полученных данных из JSON
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
