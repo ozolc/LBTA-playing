63c952115495d17c4f32c8db92df2e74759566cb
Maksim Nosov <ozolc@yandex.ru>
2019-06-26 18:19:40 +0300

    lesson2

---
+++ SlideOutMenuLBTA/SlideOutMenuLBTA/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SlideOutMenuLBTA
+//
+//  Created by Maksim Nosov on 26/06/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = UIColor.red
+        setupNavigationItems()
+    }
+    
+    @objc func handleOpen() {
+        print("Opening menu...")
+    }
+    
+    @objc func handleHide() {
+        print("Hiding menu...")
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        
+        return cell
+        
+    }
+
+}
+
