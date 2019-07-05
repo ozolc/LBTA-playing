2a8358d2d5762f8ad92853c62e433941fd08812c
Maksim Nosov <ozolc@yandex.ru>
2019-07-05 09:29:02 +0300

    lesson 05

--- SlideOutMenuLBTA/SlideOutMenuLBTA/Controllers/HomeController.swift
+++ SlideOutMenuLBTA/SlideOutMenuLBTA/Controllers/HomeController.swift
@@ -15,37 +15,60 @@ class HomeController: UITableViewController {
         
         tableView.backgroundColor = UIColor.red
         setupNavigationItems()
+        
+        setupMenuController()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        self.view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+//        print(translation)
+        
+        if gesture.state == .changed {
+            var x = translation.x
+            
+            x = min(menuWidth, x)
+            x = max(0, x)
+            
+            let transform = CGAffineTransform(translationX: x, y: 0)
+            menuController.view.transform = transform
+            navigationController?.view.transform = transform
+        
+        } else if gesture.state == .ended {
+            handleOpen()
+        }
+        
     }
     
     let menuController = MenuController()
     
     fileprivate let menuWidth: CGFloat = 300
     
+    fileprivate func performAnimations(transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            self.menuController.view.transform = transform
+            self.view.transform = transform
+//            self.navigationController?.view.transform = transform
+        })
+    }
+    
     @objc func handleOpen() {
+        performAnimations(transform: CGAffineTransform(translationX: self.menuWidth, y: 0))
+    }
+    
+    // MARK: - Fileprivate
+    @objc func handleHide() {
+        performAnimations(transform: .identity)
+    }
+    
+    fileprivate func setupMenuController() {
         menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
         let mainWindow = UIApplication.shared.keyWindow
         mainWindow?.addSubview(menuController.view)
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            
-            self.menuController.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
-            
-        })
-        
         addChild(menuController)
-        
-    }
-    
-    @objc func handleHide() {
-
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            
-            self.menuController.view.transform = .identity
-            
-        })
-        
-//        menuController.view.removeFromSuperview()
-//        menuController.removeFromParent()
     }
     
     fileprivate func setupNavigationItems() {
