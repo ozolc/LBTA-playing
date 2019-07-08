13fa4cf742a05fe439a0e8913bbc6245ad9b2381
Maksim Nosov <ozolc@yandex.ru>
2019-07-08 18:00:18 +0300

    lesson18

--- SlideOutMenuLBTA/SlideOutMenuLBTA/Controllers/Base/BaseSlidingController.swift
+++ SlideOutMenuLBTA/SlideOutMenuLBTA/Controllers/Base/BaseSlidingController.swift
@@ -43,6 +43,13 @@ class BaseSlidingController: UIViewController {
         
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         view.addGestureRecognizer(panGesture)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapDismiss))
+        darkCoverView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc fileprivate func handleTapDismiss() {
+        closeMenu()
     }
 
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
@@ -54,6 +61,7 @@ class BaseSlidingController: UIViewController {
         x = max(0, x)
         
         redViewLeadingConstraint.constant = x
+        redViewTrailingConstraint.constant = x
         darkCoverView.alpha = x / menuWidth
         
         if gesture.state == .ended {
@@ -93,18 +101,20 @@ class BaseSlidingController: UIViewController {
     func openMenu() {
         isMenuOpened = true
         redViewLeadingConstraint.constant = menuWidth
+        redViewTrailingConstraint.constant = menuWidth
         performAnimations()
     }
     
     func closeMenu() {
         redViewLeadingConstraint.constant = 0
+        redViewTrailingConstraint.constant = 0
         isMenuOpened = false
         performAnimations()
     }
     
     func didSelectMenuItem(indexPath: IndexPath) {
         performRightViewCleanUp()
-        self.closeMenu()
+        closeMenu()
         
         switch indexPath.row {
         case 0:
@@ -120,7 +130,6 @@ class BaseSlidingController: UIViewController {
             momentsController.view.backgroundColor = .orange
             let navController = UINavigationController(rootViewController: momentsController)
             navController.tabBarItem.title = "Moments"
-//            navController.tabBarItem.image
             tabBarController.viewControllers = [navController]
             rightViewController = tabBarController
         }
@@ -147,6 +156,7 @@ class BaseSlidingController: UIViewController {
     }
     
     var redViewLeadingConstraint: NSLayoutConstraint!
+    var redViewTrailingConstraint: NSLayoutConstraint!
     fileprivate let menuWidth: CGFloat = 300
     fileprivate let velocityThreshold: CGFloat = 500
     fileprivate var isMenuOpened = false
@@ -159,16 +169,18 @@ class BaseSlidingController: UIViewController {
         NSLayoutConstraint.activate([
             redView.topAnchor.constraint(equalTo: view.topAnchor),
             redView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            redView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
             blueView.topAnchor.constraint(equalTo: view.topAnchor),
-            blueView.trailingAnchor.constraint(equalTo: redView.safeAreaLayoutGuide.leadingAnchor),
+            blueView.trailingAnchor.constraint(equalTo: redView.leadingAnchor),
             blueView.widthAnchor.constraint(equalToConstant: menuWidth),
             blueView.bottomAnchor.constraint(equalTo: redView.bottomAnchor)
             ])
         
-        self.redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
         redViewLeadingConstraint.isActive = true
+        
+        redViewTrailingConstraint = redView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        redViewTrailingConstraint.isActive = true
         
         setupViewControllers()
     }
