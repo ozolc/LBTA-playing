36cb98421f3c0598f8f962c73292334685a8d312
Maksim Nosov <ozolc@yandex.ru>
2019-07-14 10:47:20 +0300

    06 - iTunes Search API JSON Fetch

--- AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -11,13 +11,42 @@ import UIKit
 class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     fileprivate let cellId = "id1234"
-
+    
+    fileprivate let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.backgroundColor = .white
         
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+        
+        fetchITunesApps()
+    }
+    
+    fileprivate func fetchITunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            
+            if let err = err {
+                print("Failed to fetch apps:", err)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+                
+            } catch let jsonErr {
+                print("Failed to decode json:", jsonErr)
+            }
+            
+            }.resume()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -29,7 +58,8 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = "HERE IS MY APP NAME"
         
         return cell
     }
---
+++ AppStoreJSONApis/AppStoreJSONApis/Model/SearchResult.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResult.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 14/07/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+    
+}
