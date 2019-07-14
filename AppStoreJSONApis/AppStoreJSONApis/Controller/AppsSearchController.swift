e04b4bb5abd50556f49780656384bff5387a404d
Maksim Nosov <ozolc@yandex.ru>
2019-07-14 13:12:34 +0300

    08 - Load Images with SDWebImage using Cocoapod

--- AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -51,11 +51,7 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
-        
-        let appResult = appResults[indexPath.item]
-        cell.nameLabel.text = appResult.trackName
-        cell.categoryLabel.text = appResult.primaryGenreName
-        cell.ratingsLabel.text = "Rating: \(appResult.averageUserRating ?? 0)"
+        cell.appResult = appResults[indexPath.item]
         
         return cell
     }
