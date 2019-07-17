aac9742c456828b01c62eb341c746e141b2bdfd5
Maksim Nosov <ozolc@yandex.ru>
2019-07-17 12:46:05 +0300

    19 - Handling User Selection

---
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
@@ -0,0 +1,16 @@
+//
+//  AppDetailController.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 17/07/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailController: BaseListController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+    }
+}
