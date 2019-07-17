c1e5f5035a8fd34adfa3e8051d60f052616bfe1a
Maksim Nosov <ozolc@yandex.ru>
2019-07-17 13:43:53 +0300

    20 - App Detail Info Cell

--- AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/AppsPageController.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/AppsPageController.swift
@@ -121,8 +121,8 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
         cell.horizontalController.didSelectHandler = { [weak self] feedResult in
             
             let controller = AppDetailController()
+            controller.appId = feedResult.id
             controller.navigationItem.title = feedResult.name
-            
             self?.navigationController?.pushViewController(controller, animated: true)
         }
         
