6a72db078323aa5c10af68483e27ffb47ae28f90
Maksim Nosov <ozolc@yandex.ru>
2019-07-18 13:00:50 +0300

    23 - App Reviews Controller

---
+++ AppStoreJSONApis/AppStoreJSONApis/Views/Apps/Details/ReviewCell.swift
@@ -0,0 +1,44 @@
+//
+//  ReviewCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 18/07/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ReviewCell: UICollectionViewCell {
+    
+    let titleLabel = UILabel(text: "Review Title", font: .boldSystemFont(ofSize: 18))
+    
+    let authorLabel = UILabel(text: "Author", font: .systemFont(ofSize: 16))
+    
+    let starsLabel = UILabel(text: "Stars", font: .systemFont(ofSize: 14))
+    
+    let bodyLabel = UILabel(text: "Review body\nReview body\nReview body\n", font: .systemFont(ofSize: 16), numberOfLines: 0)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        
+        layer.cornerRadius = 16
+        clipsToBounds = true
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            UIStackView(arrangedSubviews: [
+                titleLabel, UIView(), authorLabel
+                ]),
+            starsLabel,
+            bodyLabel
+            ], spacing: 12)
+        
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 20, left: 20, bottom: 20, right: 20))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+}
