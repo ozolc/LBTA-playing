cf9a1728527588863b764f82961d8e8b59309079
Maksim Nosov <ozolc@yandex.ru>
2019-07-19 13:03:36 +0300

    25 - JSON Custom CodingKeys

--- AppStoreJSONApis/AppStoreJSONApis/Model/Reviews.swift
+++ AppStoreJSONApis/AppStoreJSONApis/Model/Reviews.swift
@@ -20,12 +20,19 @@ struct Entry: Decodable {
     let author: Author
     let title: Label
     let content: Label
+    
+    let rating: Label
+    
+    private enum CodingKeys: String, CodingKey {
+        case author, title, content
+        case rating = "im:rating"
+    }
 }
 
 struct Author: Decodable {
     let name: Label
 }
 
-struct Label: Decodable{
+struct Label: Decodable {
     let label: String
 }
