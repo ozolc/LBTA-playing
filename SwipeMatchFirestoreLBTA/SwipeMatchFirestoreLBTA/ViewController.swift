37e2fbe17b9a7571f7098dd5eb8f5f526832b3ea
Maksim Nosov <ozolc@yandex.ru>
2019-07-24 20:39:47 +0300

    2 SwipeMatchFirestoreLBTA_02_simple_stackview_layout

---
+++ SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Maksim Nosov on 24/07/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        stackView.fillSuperview()
+    }
+
+
+}
+
