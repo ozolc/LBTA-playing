00625a9f24f3102732786dc5d08fc161785a8fc5
Maksim Nosov <ozolc@yandex.ru>
2019-07-24 21:51:37 +0300

    3 SwipeMatchFirestoreLBTA_03_view_encapsulation

--- SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/ViewController.swift
+++ SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/ViewController.swift
@@ -10,39 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let topStackView = TopNavigationStackView()
+    let buttonStackView = HomeBottomControlsStackView()
+    let blueView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let grayView = UIView()
-        grayView.backgroundColor = .gray
-        
-        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
-            let v = UIView()
-            v.backgroundColor = color
-            return v
-        }
-        
-        let topStackView = UIStackView(arrangedSubviews: subviews)
-        topStackView.distribution = .fillEqually
-        
-        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        
-        let blueView = UIView()
         blueView.backgroundColor = .blue
-        let yellowView = UIView()
-        yellowView.backgroundColor = .yellow
-        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        
-        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
-//        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        
-        view.addSubview(stackView)
-        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
-        
-        stackView.fillSuperview()
+        setupLayout()
     }
+    
+    // MARK: - Fileprivate
 
+    fileprivate func setupLayout() {
+        let overallStackView = UIStackView(arrangedSubviews: [topStackView, blueView, buttonStackView])
+        overallStackView.axis = .vertical
+        
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+    }
 
 }
 
---
+++ SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/Views/HomeBottomControlsStackView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeBottomControlsStackView.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by Maksim Nosov on 24/07/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class HomeBottomControlsStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        distribution = .fillEqually
+        heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let subviews = [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dismiss_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_circle"), #imageLiteral(resourceName: "boost_circle")].map { (img) -> UIView in
+            let button = UIButton(type: .system)
+            button.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
+            return button
+        }
+        
+        subviews.forEach({ (v) in
+            addArrangedSubview(v)
+        })
+    }
+
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
