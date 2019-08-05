3cbed351b2de88328d3c104950b94466694a6843
Maksim Nosov <ozolc@yandex.ru>
2019-08-05 09:31:54 +0300

    02 PodcastsCourseLBTA_ep2_MainTabBarController

---
+++ PodcastsCourseLBTA/PodcastsCourseLBTA/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PodcastsCourseLBTA
+//
+//  Created by Maksim Nosov on 05/08/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+    }
+
+
+}
+
