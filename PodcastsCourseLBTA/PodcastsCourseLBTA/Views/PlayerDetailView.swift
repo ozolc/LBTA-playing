83b3fc7023c33d8b6bc3cc69dffa66b0f7508d2a
Maksim Nosov <ozolc@yandex.ru>
2019-08-06 17:22:03 +0300

    15 PodcastsCourseLBTA_ep15_play_episode_avplayer

--- PodcastsCourseLBTA/PodcastsCourseLBTA/Views/PlayerDetailView.swift
+++ PodcastsCourseLBTA/PodcastsCourseLBTA/Views/PlayerDetailView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailView: UIView {
     
@@ -15,12 +16,47 @@ class PlayerDetailView: UIView {
             titleLabel.text = episode.title
             authorLabel.text = episode.author
             
+            playEpisode()
+            
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
         }
     }
     
-    @IBOutlet weak var playPauseButton: UIButton!
+    
+    fileprivate func playEpisode() {
+        print("Trying to play episode at url:", episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
+    //MARK: - IB Actions and Outlets
+    
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    
+    @objc func handlePlayPause() {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        }
+    }
+    
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel! {
@@ -28,7 +64,7 @@ class PlayerDetailView: UIView {
             titleLabel.numberOfLines = 2
         }
     }
-    
+
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
     }
