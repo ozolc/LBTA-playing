42a9154659e65bd46692cdd012b9cfb5680c9169
Maksim Nosov <ozolc@yandex.ru>
2019-12-14 20:06:05 +0300

    05 Maps Places 05 - Search UI with Combine Throttling

--- MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/Main/MainController.swift
+++ MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/Main/MainController.swift
@@ -34,11 +34,45 @@ class MainController: UIViewController {
         setupRegionForMap()
 //        setupAnnotationsForMap()
         performLocalSearch()
+        setupSearchUI()
+    }
+
+    let searchTextField = UITextField(placeholder: "Search query")
+    
+    fileprivate func setupSearchUI() {
+        
+        let whiteContainer = UIView(backgroundColor: .white)
+        view.addSubview(whiteContainer)
+        whiteContainer.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                              leading: view.leadingAnchor,
+                              bottom: nil,
+                              trailing: view.trailingAnchor,
+                              padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        
+        whiteContainer.stack(searchTextField).withMargins(.allSides(16))
+        
+        // listen for text changes and then perform new search
+        // OLD SCHOOL
+//        searchTextField.addTarget(self, action: #selector(handleSearchChanges), for: .editingChanged)
+        
+        // NEW SCHOOL Search Throttling
+        // search on the last keystroke of text changes and basically wait 500 milliseconds
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: searchTextField)
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .sink { (_) in
+                print(123123123)
+                self.performLocalSearch()
+        }
+    }
+    
+    @objc fileprivate func handleSearchChanges() {
+        performLocalSearch()
     }
     
     fileprivate func performLocalSearch() {
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = "Apple"
+        request.naturalLanguageQuery = searchTextField.text
         request.region = mapView.region
         
         let localSearch = MKLocalSearch(request: request)
@@ -49,30 +83,11 @@ class MainController: UIViewController {
             }
             
             // Success
+            // remove old annotations
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
             resp?.mapItems.forEach({ (mapItem) in
-                print(mapItem.placemark.subThoroughfare ?? "")
-                
-                let placemark = mapItem.placemark
-                var addressString = ""
-                if placemark.subThoroughfare != nil {
-                    addressString = placemark.subThoroughfare! + " "
-                }
-                if placemark.thoroughfare != nil {
-                    addressString += placemark.thoroughfare! + ", "
-                }
-                if placemark.postalCode != nil {
-                    addressString += placemark.postalCode! + " "
-                }
-                if placemark.locality != nil {
-                    addressString += placemark.locality! + ", "
-                }
-                if placemark.administrativeArea != nil {
-                    addressString += placemark.administrativeArea! + " "
-                }
-                if placemark.country != nil {
-                    addressString += placemark.country!
-                }
-                print(addressString)
+                print(mapItem.address())
                 
                 let annotation = MKPointAnnotation()
                 annotation.coordinate = mapItem.placemark.coordinate
@@ -105,6 +120,31 @@ class MainController: UIViewController {
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: centerCoordinate, span: span)
         mapView.setRegion(region, animated: true)
+    }
+}
+
+extension MKMapItem {
+    func address() -> String {
+        var addressString = ""
+        if placemark.subThoroughfare != nil {
+            addressString = placemark.subThoroughfare! + " "
+        }
+        if placemark.thoroughfare != nil {
+            addressString += placemark.thoroughfare! + ", "
+        }
+        if placemark.postalCode != nil {
+            addressString += placemark.postalCode! + " "
+        }
+        if placemark.locality != nil {
+            addressString += placemark.locality! + ", "
+        }
+        if placemark.administrativeArea != nil {
+            addressString += placemark.administrativeArea! + " "
+        }
+        if placemark.country != nil {
+            addressString += placemark.country!
+        }
+        return addressString
     }
 }
 
