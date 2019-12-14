2d860708a67241e05eb8a309ec0f993b9b91550e
Maksim Nosov <ozolc@yandex.ru>
2019-12-14 10:32:28 +0300

    02 Maps 02 - SwiftUI Preview and MapView Regions

--- MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/Main/MainController.swift
+++ MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/Main/MainController.swift
@@ -20,7 +20,35 @@ class MainController: UIViewController {
         view.addSubview(mapView)
         mapView.fillSuperview()
         
-        mapView.mapType = .hybrid
+        setupRegionForMap()
+    }
+    
+    fileprivate func setupRegionForMap() {
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: centerCoordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+// SwiftUI Preview
+import SwiftUI
+
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> MainController {
+            return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainController, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+            
+        }
+        
+        typealias UIViewControllerType = MainController
         
     }
 }
