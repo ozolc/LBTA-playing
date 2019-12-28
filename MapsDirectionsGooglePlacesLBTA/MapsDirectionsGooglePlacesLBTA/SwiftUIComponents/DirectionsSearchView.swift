b960f3458f27828791a7c3ee33498c990fa8fdb4
Maksim Nosov <ozolc@yandex.ru>
2019-12-28 13:22:46 +0300

    38. Combine Publishers CombineLatest

--- MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/SwiftUIComponents/DirectionsSearchView.swift
+++ MapsDirectionsGooglePlacesLBTA/MapsDirectionsGooglePlacesLBTA/SwiftUIComponents/DirectionsSearchView.swift
@@ -10,13 +10,53 @@ import SwiftUI
 import MapKit
 
 struct DirectionsMapView: UIViewRepresentable {
+    
+    @EnvironmentObject var env: DirectionsEnvironment
+    
     typealias UIViewType = MKMapView
     
-    func makeUIView(context: UIViewRepresentableContext<DirectionsMapView>) -> DirectionsMapView.UIViewType {
-        MKMapView()
+    let mapView = MKMapView()
+    
+    func makeCoordinator() -> DirectionsMapView.Coordinator {
+        return Coordinator(mapView: mapView)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        init(mapView: MKMapView) {
+            super.init()
+            mapView.delegate = self
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .red
+            renderer.lineWidth = 5
+            return renderer
+        }
+        
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<DirectionsMapView>) -> MKMapView {
+        mapView
     }
     
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<DirectionsMapView>) {
+        
+        uiView.removeOverlays(uiView.overlays)
+        uiView.removeAnnotations(uiView.annotations)
+        
+        [env.sourceMapItem, env.destinationMapItem].compactMap{$0}.forEach { (mapItem) in
+            let annotation = MKPointAnnotation()
+            annotation.title = mapItem.name
+            annotation.coordinate = mapItem.placemark.coordinate
+            uiView.addAnnotation(annotation)
+        }
+        uiView.showAnnotations(uiView.annotations, animated: false)
+        
+        if let route = env.route {
+            uiView.addOverlay(route.polyline)
+        }
         
     }
 }
@@ -33,50 +73,43 @@ struct SelectLocationView: View {
     var body: some View {
         VStack {
             
-            HStack(spacing: 16) {
+            HStack (spacing: 16) {
                 Button(action: {
-                    // need to dismiss this view somehow
                     self.env.isSelectingSource = false
                     self.env.isSelectingDestination = false
+                    
                 }, label: {
                     Image(uiImage: #imageLiteral(resourceName: "back_arrow"))
                 }).foregroundColor(.black)
                 
-                TextField("Enter earch term", text: $searchQuery)
-                    .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification)
-                        .delay(for: .milliseconds(500),
-                               scheduler: RunLoop.main)) { _ in
-                                
-                                // search
-                                let request = MKLocalSearch.Request()
-                                request.naturalLanguageQuery = self.searchQuery
-                                let search = MKLocalSearch(request: request)
-                                search.start { (resp, err) in
-                                    // check your err
-                                    if let err = err {
-                                        print("Failed to search", err)
-                                    }
-                                    
-                                    self.mapItems = resp?.mapItems ?? []
-                                }
-                                
+                TextField("Enter search term", text: $searchQuery)
+                    .onReceive(NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification).debounce(for: .milliseconds(500), scheduler: RunLoop.main)) { _ in
+                    
+                        // search
+                        let request = MKLocalSearch.Request()
+                        request.naturalLanguageQuery = self.searchQuery
+                        let search = MKLocalSearch(request: request)
+                        search.start { (resp, err) in
+                            // check your error
+                            self.mapItems = resp?.mapItems ?? []
+                        }
                 }
             }.padding()
             
             if mapItems.count > 0 {
                 ScrollView {
-                    ForEach (mapItems, id: \.self) { item in
+                    ForEach(mapItems, id: \.self) { item in
                         Button(action: {
                             if self.env.isSelectingSource {
                                 self.env.isSelectingSource = false
-                                self.env.sourceSourceMapItem = item
+                                self.env.sourceMapItem = item
                             } else {
                                 self.env.isSelectingDestination = false
                                 self.env.destinationMapItem = item
                             }
                         }) {
                             HStack {
-                                VStack(alignment: .leading){
+                                VStack (alignment: .leading) {
                                     Text("\(item.name ?? "")")
                                         .font(.headline)
                                     Text("\(item.address())")
@@ -92,9 +125,6 @@ struct SelectLocationView: View {
             Spacer()
         }
         .edgesIgnoringSafeArea(.bottom)
-        .onAppear(perform: {
-            
-        })
         .navigationBarTitle("")
         .navigationBarHidden(true)
     }
@@ -117,33 +147,28 @@ struct DirectionsSearchView: View {
                             NavigationLink(destination: SelectLocationView(), isActive: $env.isSelectingSource) {
                                 
                                 HStack {
-                                    Text(env.sourceSourceMapItem != nil ? (env.sourceSourceMapItem?.name ?? "") : "Source") // come from Environment object
+                                    Text(env.sourceMapItem != nil ? (env.sourceMapItem?.name ?? "") : "Source")
                                     Spacer()
                                 }
-                                .padding()
-                                .background(Color.white)
-                                .cornerRadius(3)
+                                .padding() .background(Color.white).cornerRadius(3)
                             }
-                            
                         }
                         
                         HStack(spacing: 16) {
-                            Image(uiImage: #imageLiteral(resourceName: "annotation_icon")
-                                .withRenderingMode(.alwaysTemplate))
-                                .foregroundColor(.white)
+                            Image(uiImage: #imageLiteral(resourceName: "annotation_icon").withRenderingMode(.alwaysTemplate)).foregroundColor(.white)
                                 .frame(width: 24)
+                            
                             
                             NavigationLink(destination: SelectLocationView(), isActive: $env.isSelectingDestination) {
                                 
                                 HStack {
-                                    Text(env.destinationMapItem != nil ? (env.destinationMapItem?.name ?? "") : "Destination") // come from Environment object
+                                    Text(env.destinationMapItem != nil ? (env.destinationMapItem?.name ?? "") : "Destination")
                                     Spacer()
                                 }
-                                .padding()
-                                .background(Color.white)
-                                .cornerRadius(3)
+                                .padding() .background(Color.white).cornerRadius(3)
                             }
                         }
+                        
                     }
                     .padding()
                     .background(Color.blue)
@@ -152,25 +177,51 @@ struct DirectionsSearchView: View {
                 }
                 
                 // status bar cover
-                Spacer().frame(
-                    width: UIApplication.shared.windows.filter{ $0.isKeyWindow }.first?.frame.width,
-                    height: UIApplication.shared.windows.filter{ $0.isKeyWindow }.first?.safeAreaInsets.top)
+                Spacer().frame(width: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.frame.width, height: UIApplication.shared.windows.filter{$0.isKeyWindow}.first?.safeAreaInsets.top)
                     .background(Color.blue)
                     .edgesIgnoringSafeArea(.top)
             }
-            .navigationBarTitle("DIRECTIONS")
+        .navigationBarTitle("DIRECTIONS")
             .navigationBarHidden(true)
         }
     }
 }
+
+import Combine
 
 // treat your env as the brain of your application
 class DirectionsEnvironment: ObservableObject {
     @Published var isSelectingSource = false
     @Published var isSelectingDestination = false
     
-    @Published var sourceSourceMapItem: MKMapItem?
+    @Published var sourceMapItem: MKMapItem?
     @Published var destinationMapItem: MKMapItem?
+    
+    @Published var route: MKRoute?
+    
+    var cancellable: AnyCancellable?
+    
+    init() {
+        // listen for changes on sourceMapItem, destinationMapitem
+        cancellable = Publishers.CombineLatest($sourceMapItem, $destinationMapItem).sink { (items) in
+//            print(items.0 ?? "", items.1 ?? "")
+            
+            // searching for directions
+            let request = MKDirections.Request()
+            request.source = items.0
+            request.destination = items.1
+            let directions = MKDirections(request: request)
+            directions.calculate { [weak self] (resp, err) in
+                if let err = err {
+                    print("Failed to calculate directions:", err)
+                    return
+                }
+                
+//                print(resp?.routes.first ?? "")
+                self?.route = resp?.routes.first
+            }
+        }
+    }
 }
 
 struct DirectionsSearchView_Previews: PreviewProvider {
