38aaee69e88134012e0f65faed4b3075968d558d
Maksim Nosov <ozolc@yandex.ru>
2020-02-12 13:50:26 +0200

    02 - Setting up the Foundation with UITabBarController

---
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 12.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
