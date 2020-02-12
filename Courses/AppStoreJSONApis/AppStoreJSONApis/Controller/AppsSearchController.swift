99283b39a6e348a69e5ff099794bcb8419890bbe
Maksim Nosov <ozolc@yandex.ru>
2020-02-12 15:27:45 +0200

    04 - Cells and UIStackView Layouts

--- Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class AppsSearchController: UICollectionViewController {
+class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
     }
     
     init() {
