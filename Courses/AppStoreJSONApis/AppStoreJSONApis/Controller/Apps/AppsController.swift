90aa21c6ac08a5d0d1eca806da025213b909b488
Maksim Nosov <ozolc@yandex.ru>
2020-02-13 15:09:34 +0200

    10 - Nested UICollectionViews

---
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/AppsController.swift
@@ -0,0 +1,34 @@
+//
+//  AppsController.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 13.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "id"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+        
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+}
--- Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Search/AppsSearchController.swift
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Search/AppsSearchController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SDWebImage
 
-class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
+class AppsSearchController: BaseListController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     fileprivate let cellId = "cellId"
     
@@ -97,14 +97,6 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         cell.appResult = appResults[indexPath.item]
         
         return cell
-    }
-    
-    init() {
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
 }
