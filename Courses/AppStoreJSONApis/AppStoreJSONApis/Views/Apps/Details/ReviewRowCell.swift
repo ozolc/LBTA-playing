900f8429f8b502682e115a29e644974e4a442862
Maksim Nosov <ozolc@yandex.ru>
2020-02-15 11:28:44 +0200

    24 - iTunes Reviews JSON

---
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Views/Apps/Details/ReviewRowCell.swift
@@ -0,0 +1,28 @@
+//
+//  ReviewRowCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 15.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ReviewRowCell: UICollectionViewCell {
+    
+    let reviewsController = ReviewsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        addSubview(reviewsController.view)
+        reviewsController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
