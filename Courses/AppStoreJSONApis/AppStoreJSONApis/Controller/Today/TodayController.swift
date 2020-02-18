06b2113636d142cac9d6ec439ace0b2f5dce9355
Maksim Nosov <ozolc@yandex.ru>
2020-02-18 07:58:13 +0200

    32 - Dynamic Today Cells

--- Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Today/TodayController.swift
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Today/TodayController.swift
@@ -12,6 +12,11 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     
     fileprivate let cellId = "cellId"
     
+    let items = [
+        TodayItem.init(category: "LIFE HACK", title: "Utilizing your Time", image: #imageLiteral(resourceName: "garden"), description: "All the tools and apps you need to intelligently organize your life the right way.", backgroundColor: .white),
+        TodayItem.init(category: "HOLIDAYS", title: "Travel on a Budget", image: #imageLiteral(resourceName: "holiday"), description: "Find out all you need to know on how to travel without packing everything!", backgroundColor: #colorLiteral(red: 0.9838578105, green: 0.9588007331, blue: 0.7274674177, alpha: 1))
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +27,6 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
         collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
     }
     
-    var startingFrame: CGRect?
     var appFullscreenController: AppFullscreenController!
     
     var topConstraint: NSLayoutConstraint?
@@ -33,91 +37,81 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         let appFullscreenController = AppFullscreenController()
-        let redView = appFullscreenController.view!
-        redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
-        view.addSubview(redView)
-        
+        appFullscreenController.todayItem = items[indexPath.row]
+        appFullscreenController.dismissHandler = {
+            self.handleRemoveRedView()
+        }
+        let fullscreenView = appFullscreenController.view!
+        view.addSubview(fullscreenView)
+
         addChild(appFullscreenController)
         
         self.appFullscreenController = appFullscreenController
         
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
         
-        // absolute coordinates of cell
+        // absolute coordindates of cell
         guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
         
         self.startingFrame = startingFrame
-        
+
         // auto layout constraint animations
         // 4 anchors
-        redView.translatesAutoresizingMaskIntoConstraints = false
-        topConstraint = redView.topAnchor.constraint(
-            equalTo: view.topAnchor, constant: startingFrame.origin.y)
-        
-        leadingConstraint = redView.leadingAnchor.constraint(
-            equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
-        
-        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
-        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+        fullscreenView.translatesAutoresizingMaskIntoConstraints = false
+        topConstraint = fullscreenView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = fullscreenView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = fullscreenView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = fullscreenView.heightAnchor.constraint(equalToConstant: startingFrame.height)
         
         [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
         self.view.layoutIfNeeded()
         
-        redView.layer.cornerRadius = 16
+        fullscreenView.layer.cornerRadius = 16
         
-        UIView.animate(withDuration: 0.7,
-                       delay: 0,
-                       usingSpringWithDamping: 0.7,
-                       initialSpringVelocity: 0.7,
-                       options: .curveEaseOut,
-                       animations: {
-                        
-                        self.topConstraint?.constant = 0
-                        self.leadingConstraint?.constant = 0
-                        self.widthConstraint?.constant = self.view.frame.width
-                        self.heightConstraint?.constant = self.view.frame.height
-                        
-                        self.view.layoutIfNeeded() // start animation
-                        
-                        self.tabBarController?.tabBar.frame.origin.y = self.view.frame.height+100
-                        
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
+            
+            self.view.layoutIfNeeded() // starts animation
+
+            self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
+
         }, completion: nil)
-        
     }
     
-    @objc fileprivate func handleRemoveRedView(gesture: UITapGestureRecognizer) {
-        UIView.animate(withDuration: 0.7,
-                       delay: 0,
-                       usingSpringWithDamping: 0.7,
-                       initialSpringVelocity: 0.7,
-                       options: .curveEaseOut,
-                       animations: {
-                        
-                        self.appFullscreenController.tableView.contentOffset = .zero
-                        
-                        guard let startingFrame = self.startingFrame else { return }
-                        self.topConstraint?.constant = startingFrame.origin.y
-                        self.leadingConstraint?.constant = startingFrame.origin.x
-                        self.widthConstraint?.constant = startingFrame.width
-                        self.heightConstraint?.constant = startingFrame.height
-                        
-                        self.view.layoutIfNeeded()
-                        
-                        self.tabBarController?.tabBar.frame.origin.y = self.view.frame.height-80
-                        
+    var startingFrame: CGRect?
+    
+    @objc func handleRemoveRedView() {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+            
+            self.appFullscreenController.tableView.contentOffset = .zero
+            
+            guard let startingFrame = self.startingFrame else { return }
+            self.topConstraint?.constant = startingFrame.origin.y
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
+            
+            self.view.layoutIfNeeded()
+            
+            self.tabBarController?.tabBar.transform = .identity
+            
         }, completion: { _ in
-            gesture.view?.removeFromSuperview()
+            self.appFullscreenController.view.removeFromSuperview()
             self.appFullscreenController.removeFromParent()
         })
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return items.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! TodayCell
-        
+        cell.todayItem = items[indexPath.item]
         return cell
     }
     
@@ -132,4 +126,5 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return .init(top: 32, left: 0, bottom: 32, right: 0)
     }
+    
 }
---
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Model/TodayItem.swift
@@ -0,0 +1,19 @@
+//
+//  TodayItem.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 18.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+struct TodayItem {
+    
+    let category: String
+    let title: String
+    let image: UIImage
+    let description: String
+    let backgroundColor: UIColor
+    
+}
