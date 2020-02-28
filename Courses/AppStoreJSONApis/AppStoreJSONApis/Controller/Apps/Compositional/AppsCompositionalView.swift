3910ba536f569fd84f0096de1eacbf540370b0c6
Maksim Nosov <ozolc@yandex.ru>
2020-02-28 18:46:53 +0200

    49 - Preparing SwiftUI Preview

---
+++ Courses/AppStoreJSONApis/AppStoreJSONApis/Controller/Apps/Compositional/AppsCompositionalView.swift
@@ -0,0 +1,62 @@
+//
+//  AppsCompositionalView.swift
+//  AppStoreJSONApis
+//
+//  Created by Maksim Nosov on 28.02.2020.
+//  Copyright © 2020 Maksim Nosov. All rights reserved.
+//
+
+import SwiftUI
+
+class CompositionalController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView.backgroundColor = .systemBackground
+        navigationItem.title = "Apps"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+struct AppsView: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<AppsView>) -> UIViewController {
+        let redController = CompositionalController()
+        return UINavigationController(rootViewController: redController)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<AppsView>) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
+    
+    
+}
+
+struct AppsCompositionalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppsView()
+            .edgesIgnoringSafeArea(.all)
+        
+    }
+}
